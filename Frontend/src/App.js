c903202dbe0b45dca4c68d5836642d917d05cdaa
mayank74pathak <56240383+mayank74pathak@users.noreply.github.com>
2025-04-24 19:06:09 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Products from './components/Products';
-import InsertProduct from './components/InsertProduct'
+import InsertProduct from './components/InsertProduct';
 import UpdateProduct from './components/UpdateProduct';
 import About from './components/About';
 
@@ -12,28 +12,21 @@ import {
   Route
 } from 'react-router-dom';
 
-
-
-
 function App() {
   return (
-    <div className="App">
-      <Navbar title="IMS" about="About"></Navbar>
+    <Router>
+      <div className="App">
+        <Navbar title="IMS" about="About" />
 
-      <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/insertproduct" element={<InsertProduct />} />
           <Route path="/updateproduct/:id" element={<UpdateProduct />} />
           <Route path="/about" element={<About />} />
-
         </Routes>
-
-      </Router>
-
-
-    </div>
+      </div>
+    </Router>
   );
 }
 
