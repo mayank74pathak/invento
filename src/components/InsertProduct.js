2fda5689a854eb57c42d4666f81c31f9640b2b74
mayank74pathak <56240383+mayank74pathak@users.noreply.github.com>
2025-04-25 16:26:01 +0530

    Update InsertProduct.js

--- src/components/InsertProduct.js
+++ src/components/InsertProduct.js
@@ -34,7 +34,7 @@ export default function InsertProduct() {
         setError("");
 
         try {
-            const res = await fetch("http://localhost:3001/insertproduct", {
+            const res = await fetch("https://inventobackend-1.onrender.com/insertproduct", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
